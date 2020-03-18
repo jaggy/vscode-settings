a462d222e81e20ebeff6684c94d6f9507f605122
Jaggy Gauran <jaggy@humans.ph>
2020-03-18 11:47:48 +0800

    Add a setup script to install the vscode extensions I need.

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#/usr/bin/bash
+
+# The command to list all code extensions:
+#     code --install-extension
+
+extensions=(
+  be5invis.vscode-custom-css
+  bmewburn.vscode-intelephense-client
+  cpylua.language-postcss
+  dbaeumer.vscode-eslint
+  dkundel.vscode-new-file
+  EditorConfig.EditorConfig
+  esbenp.prettier-vscode
+  ikappas.phpcs
+  imagio.vscode-dimmer-block
+  JakeBecker.elixir-ls
+  lehni.vscode-fix-checksums
+  MartyBeGood.single-editor-tabs
+  ms-azuretools.vscode-docker
+  octref.vetur
+  onecentlin.laravel-blade
+  Prisma.vscode-graphql
+  sleistner.vscode-fileutils
+  teabyii.ayu
+  vscodevim.vim
+  wwm.better-align
+)
+
+for extension in ${extensions[*]}; do
+  code --install-extension $extension
+done
